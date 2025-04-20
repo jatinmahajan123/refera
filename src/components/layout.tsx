129f90d42bbcc2152a0807b3bd290ff509cadb40
jatin <jatinmahajan1713@gmail.com>
2025-04-21 02:39:29 +0530

    updated dashboard sidebar1

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,11 @@
-import Sidebar from './SideBar';
+import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
+
       <div className="flex-1 overflow-y-auto">
         <Outlet />
       </div>
